75a98a8eca92d3a8cbd65baa4a90292a76c6effe
Hlib Khoruznhyi <gleb5192005@gmail.com>
2025-09-13 09:49:29 +0200

    Data Validation component

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 import sys
 
 from src.components.data_ingestion import DataIngestion
+from src.components.data_validation import DataValidaton
 from src.exception.exception import NetworkSecurityException
 from src.logging.logger import logging
-from src.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig
-from src.entity.artifact_entity import DataIngestionArtifact
+from src.entity.config_entity import (
+    DataIngestionConfig,
+    TrainingPipelineConfig,
+    DataValidaionConfig,
+)
+from src.entity.artifact_entity import DataIngestionArtifact, DataValidatinArtifact
 
 if __name__ == "__main__":
     try:
-        data_ingestion = DataIngestion(DataIngestionConfig(TrainingPipelineConfig()))
+        training_pipeline_config = TrainingPipelineConfig()
+        data_ingesion_config = DataIngestionConfig(training_pipeline_config)
+        data_ingestion = DataIngestion(data_ingesion_config)
         logging.info("Initiate the data ingestion")
         data_ingestion_artifact = data_ingestion.initate_data_ingestion()
+        logging.info("Data ingestion initiation completed")
         print(data_ingestion_artifact)
+        data_validation_config = DataValidaionConfig(training_pipeline_config)
+        data_validation = DataValidaton(data_ingestion_artifact, data_validation_config)
+        logging.info("Initiate the data validation")
+        data_validation_arifact = data_validation.initiate_data_validation()
+        logging.info("Data validation initiation completed")
+        print(data_validation_arifact)
     except Exception as e:
         raise NetworkSecurityException(e, sys)
