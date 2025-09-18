fb66f507d47ee4e98b570266c9fd42ccae09ac65
Hlib Khoruznhyi <gleb5192005@gmail.com>
2025-09-18 20:49:14 +0200

    added README.md

--- README.md
+++ README.md
@@ -1,1 +1,77 @@
-# Network Security Project For Phising Data
+# Network Security Project For Phishing Data
+A Python project for phishind data analysis and prediction
+
+## Table of Content
+- [About](#about)
+- [Technologies](#technologies)
+- [Instalation](#instalation)
+- [Environment Variables](#environment-variables)
+- [Usage](#usage)
+- [Project Structure](#project-structure)
+- [Licesnce](#license)
+
+## About
+This project analyzes network data to detect phishing activities. It includes data preprocessing, model training and prediction functionalities.
+
+## Technologies
+- Python 3.12.9
+- FastAPI (For API in `app.py`)
+- Pandas, NumPy, Scikit-learn
+- Docker
+
+## Instalation 
+1. Clone the repository:
+```bash
+git clone https://github.com/Exzustic/networksecurity
+cd networksecurity
+```
+2. Create a virtual environment and activate it:
+```bash
+python -m venv venv 
+source venv\bin\activate # Linux/Mac
+venv\Scripts\activate # Windows 
+```
+3. Install dependencies:
+```bash
+pip install -r requirements.txt
+```
+
+## Environment Variables 
+Create `.env` file in the root folder and required variables:
+```env
+MONGO_DB_URL = your_mongo_db_url
+```
+or you can choose not to do this
+
+## Usage
+- To run the API:
+```bash
+uvicorn app:app
+```
+- To run the main processing script:
+```bash
+python main.py
+```
+
+## Project Structure
+```
+networksecurity/
+|
+├── src/                # Source code
+├── data_schema/        # schema of dataset
+├── final_model/        # Trained model
+├── Network_Data/       # Raw data (works when you don't have .env)
+├── valid_data/         # Validation dataset
+├── prediction_output/  # Prediction result
+├── templates/          # HTML templates for API
+├── log/                # Logs
+├── app.py              # FastAPI API entry point 
+├── main.py             # Main processing script
+├── requirements.txt
+├── Dockerfile
+```
+
+## License 
+MIT License  
+Copyright (c) 2025 Krish Naik
+
--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -17,6 +17,7 @@ import pymongo
 from sklearn.model_selection import train_test_split
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
 MONGO_DB_URL = os.getenv("MONGO_DB_URL")
@@ -25,11 +26,11 @@ MONGO_DB_URL = os.getenv("MONGO_DB_URL")
 class DataIngestion:
     def __init__(self, data_ingestion_config: DataIngestionConfig):
         try:
-            self.data_ingestion_config=data_ingestion_config
+            self.data_ingestion_config = data_ingestion_config
         except Exception as e:
             raise NetworkSecurityException(e, sys)
 
-    def export_collection_as_dataframe(self):
+    def import_collection_as_dataframe(self):
         """
         Read data from mongodb
         """
@@ -42,12 +43,20 @@ class DataIngestion:
             df = pd.DataFrame(list(collection.find()))
             if "_id" in df.columns.to_list():
                 df = df.drop(columns=["_id"], axis=1)
-            
-            df = df.replace({"na":np.nan})
+
+            df = df.replace({"na": np.nan})
             return df
         except Exception as e:
             raise NetworkSecurityException(e, sys)
-        
+
+    def import_local_dataframe(self):
+        try:
+            df = pd.read_csv("Network_Data/phishingData.csv")
+            df = df.replace({"na": np.nan})
+            return df
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+
     def export_data_into_feature_store(self, dataframe: pd.DataFrame):
         try:
             feature_store_file_path = self.data_ingestion_config.feature_store_file_path
@@ -66,29 +75,40 @@ class DataIngestion:
             )
 
             logging.info("Perfomed train test split on the dataframe")
-            logging.info('Exited split_data_as_train_test method of Data_Ingestion class')
+            logging.info(
+                "Exited split_data_as_train_test method of Data_Ingestion class"
+            )
 
             dir_path = os.path.dirname(self.data_ingestion_config.training_file_path)
             os.makedirs(dir_path, exist_ok=True)
-            
-            logging.info("Exporting train and test file path.")
-            train_set.to_csv(self.data_ingestion_config.training_file_path, index=False, header=True)
 
-            test_set.to_csv(self.data_ingestion_config.testing_file_path, index=False, header=True)
+            logging.info("Exporting train and test file path.")
+            train_set.to_csv(
+                self.data_ingestion_config.training_file_path, index=False, header=True
+            )
+
+            test_set.to_csv(
+                self.data_ingestion_config.testing_file_path, index=False, header=True
+            )
             logging.info("Exported train and test file path.")
         except Exception as e:
             raise NetworkSecurityException(e, sys)
 
     def initate_data_ingestion(self):
         try:
-            dataframe = self.export_collection_as_dataframe()
+
+            if MONGO_DB_URL:
+                dataframe = self.import_collection_as_dataframe()
+            else:
+                logging.info("MONGO_DB_URL not found. Importing data from local storage")
+                dataframe = self.import_local_dataframe()
             dataframe = self.export_data_into_feature_store(dataframe)
             self.split_data_as_train_test(dataframe)
 
             data_ingestin_artifact = DataIngestionArtifact(
-                train_file_path = self.data_ingestion_config.training_file_path,
-                test_file_path = self.data_ingestion_config.testing_file_path
-                )
+                train_file_path=self.data_ingestion_config.training_file_path,
+                test_file_path=self.data_ingestion_config.testing_file_path,
+            )
             return data_ingestin_artifact
         except Exception as e:
             raise NetworkSecurityException(e, sys)
